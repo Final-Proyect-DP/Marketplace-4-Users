1adc010d8119fd36f7030593d5ac04a77f19612c
KennethTaipe06 <kataipeg@uce.edu.ec>
2025-02-05 18:20:51 -0500

    feat: :sparkles: configure CORS middleware with custom options

--- app.js
+++ app.js
@@ -72,7 +72,11 @@ redisClient.on('connect', () => {
   console.log('Connected to Redis');
 });
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use((req, res, next) => {
   req.redisClient = redisClient;
